ad251dccca45c82d8d9d67b926695fd779ec79e9
Mike <42689366+MikeIvanichev@users.noreply.github.com>
2024-11-27 23:13:05 +0200

    fix encoders

---
+++ keyboards/ocreeb12/config.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#define ENCODER_A_PINS {GP28, GP18}
+#define ENCODER_B_PINS {GP27, GP20}
--- keyboards/ocreeb12/keymaps/vial/keymap.c
+++ keyboards/ocreeb12/keymaps/vial/keymap.c
@@ -12,7 +12,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 #ifdef ENCODER_MAP_ENABLE
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [0] =   { ENCODER_CCW_CW(ENC01, ENC02), ENCODER_CCW_CW(ENC11, ENC12)
+    [0] =   { ENCODER_CCW_CW(ENC01, ENC02), ENCODER_CCW_CW(ENC10, ENC11)
     }
 };
 #endif
