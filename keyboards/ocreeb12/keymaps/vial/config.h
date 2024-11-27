d71c13566b6b7b6e5027b70cfc1325f771eac5c4
Mike <42689366+MikeIvanichev@users.noreply.github.com>
2024-11-27 02:14:36 +0200

    add ocreeb12

---
+++ keyboards/ocreeb12/keymaps/vial/config.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xA7, 0xF0, 0xF5, 0xD5, 0x6A, 0x7F, 0xE9, 0xF1}
+#define VIAL_UNLOCK_COMBO_ROWS {1}
+#define VIAL_UNLOCK_COMBO_COLS {0}
---
+++ keyboards/ocreeb12/keymaps/vial/macros.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include QMK_KEYBOARD_H
+
+// MACROS ROW 1
+#define M00 KC_NO
+#define M01 KC_NO
+#define M02 KC_NO
+#define M03 KC_NO
+
+// MACROS ROW 2
+#define M10 KC_NO
+#define M11 KC_NO
+#define M12 KC_NO
+#define M13 KC_NO
+
+// MACROS ROW 3
+#define M20 KC_NO
+#define M21 KC_NO
+#define M22 KC_NO
+#define M23 KC_NO
+
+// MACROS ENCODER 1
+#define ENC01 KC_F13
+#define ENC02 KC_F14
+#define ENC03 KC_F15
+
+// MACROS ENCODER 2
+#define ENC10 KC_F16
+#define ENC11 KC_F17
+#define ENC12 KC_F18
